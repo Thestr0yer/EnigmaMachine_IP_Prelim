3995e1f9f71303aea5506030c4216871217ac815
Thestr0yer <gabbybelo5@gmail.com>
2025-03-26 07:25:30 +0800

    Add project files.

---
+++ EnigmaMachine_IP_Prelim/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace EnigmaMachine_IP_Prelim
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
